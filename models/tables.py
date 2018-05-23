4b9a05ab88e40452399e786340ffd4f65baf58ea
Justin Liu <justinliu2015@gmail.com>
2018-05-23 15:58:53 -0700

    Initial backend commit

--- models/tables.py
+++ models/tables.py
@@ -7,25 +7,25 @@
 # There is an implicit 'id integer autoincrement' field
 # Consult manual for more options, validators, etc.
 
-import datetime
+import date
 
 def get_user_email():
     return auth.user.email if auth.user is not None else None
 
 
-db.define_table('checklist',
+db.define_table('transaction',
                 Field('user_email', default=get_user_email()),
-                Field('title'),
-                Field('is_public', 'boolean', default=False),
-                Field('memo', 'text'),
-                Field('updated_on', 'datetime', update=datetime.datetime.utcnow())
+                Field('date_made', 'date'),
+                Field('price', 'float'),
+                Field('description', 'text')
                 )
 
-db.checklist.user_email.writable = False
-db.checklist.user_email.readable = False
-db.checklist.updated_on.writable = db.checklist.updated_on.readable = False
-db.checklist.id.writable = db.checklist.id.readable = False
-db.checklist.is_public.writable = db.checklist.is_public.readable = False
+db.transaction.user_email.writable = False
+db.transaction.user_email.readable = False
+db.transaction.date_made.writable = db.transaction.date_made.readable = False
+db.transaction.id.writable = db.transaction.id.readable = False
+db.transaction.price.writable = db.transaction.price.readable = False
+db.transaction.description.writable = db.transaction.description.readable = False
 
 
 # after defining tables, uncomment below to enable auditing
