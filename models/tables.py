739f444f2a6e1f03ba7b9f12fd6da89d2bac130f
Josh <JTCheung3@gmail.com>
2018-05-23 16:22:35 -0700

    cleaning files

--- models/tables.py
+++ models/tables.py
@@ -7,25 +7,25 @@
 # There is an implicit 'id integer autoincrement' field
 # Consult manual for more options, validators, etc.
 
-import date
+import datetime
 
 def get_user_email():
     return auth.user.email if auth.user is not None else None
 
 
-db.define_table('transaction',
+db.define_table('transactions',
                 Field('user_email', default=get_user_email()),
                 Field('date_made', 'date'),
                 Field('price', 'float'),
                 Field('description', 'text')
                 )
 
-db.transaction.user_email.writable = False
-db.transaction.user_email.readable = False
-db.transaction.date_made.writable = db.transaction.date_made.readable = False
-db.transaction.id.writable = db.transaction.id.readable = False
-db.transaction.price.writable = db.transaction.price.readable = False
-db.transaction.description.writable = db.transaction.description.readable = False
+db.transactions.user_email.writable = False
+db.transactions.user_email.readable = False
+db.transactions.date_made.writable = db.transactions.date_made.readable = False
+db.transactions.id.writable = db.transactions.id.readable = False
+db.transactions.price.writable = db.transactions.price.readable = False
+db.transactions.description.writable = db.transactions.description.readable = False
 
 
 # after defining tables, uncomment below to enable auditing
