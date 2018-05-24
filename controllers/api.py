6750ea85a9b04ecf846dd67bad906d72c7e75010
Justin Liu <justinliu2015@gmail.com>
2018-05-23 19:10:09 -0700

    Insert to db works, trying to make it efficient

--- controllers/api.py
+++ controllers/api.py
@@ -1,1 +1,8 @@
 # Here go your api methods.
+def add_transaction():
+    t_id = db.transactions.insert(
+        date = request.vars.date,
+        price = request.vars.price,
+        description = request.vars.description
+    )
+    return 'ok'
