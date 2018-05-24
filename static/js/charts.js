696d20eb8f43c2b259896d70217be01ffdde0f6d
Justin Liu <justinliu2015@gmail.com>
2018-05-24 16:18:12 -0700

    Revert "update"
    This reverts commit a9ef89e39155f6c251f340737e9688476baf7669.

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,14 +1,12 @@
 
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
-    height: 100,
-    width:200,
-    type: 'line',
+    type: 'bar',
     data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
-            label: 'Money Spent',
-            data: [12, 19, 8, 5, 25, 32, 15, 6, 20, 38, 9, 10],
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
