f90df2c3732d724ed3b5578f2fd2dcbca0795df1
Justin Liu <justinliu2015@gmail.com>
2018-05-24 15:30:36 -0700

    Old

--- controllers/api.py
+++
@@ -1,8 +0,0 @@
-# Here go your api methods.
-def add_transaction():
-    t_id = db.transactions.insert(
-        date = request.vars.date,
-        price = request.vars.price,
-        description = request.vars.description
-    )
-    return 'ok'
