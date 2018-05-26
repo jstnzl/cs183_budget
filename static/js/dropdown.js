8fe485704f65ed1f16b2d9549a8a75de2091112e
Justin <justinliu2015@gmail.com>
2018-05-25 17:16:02 -0700

    Dropdown basic functionality

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,19 +1,328 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+
+function selectPrices(value){
+  var x = document.getElementById("prices").value;
+  //console.log(x);
+  switch(x){
+    case "1":
+        lessThanFive();
+          break;
+    case "2":
+        fiveToTen();
+          break;
+    case "3":
+        elevenToTwenty();
+          break;
+    case "4":
+        twentyOneToFifty();
+          break;
+    case "5":
+        fiftyOneToHundred();
+          break;
+    case "6":
+        greaterThanHundred();
+          break;
+    case "7":
+        income();
+          break;
+    default:
+        allPrice();
+          break;
+    }
+  }
+
+  function allPrice(){
+    table = document.getElementById("list");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        var value = td.innerHTML.substring(1, td.innerHTML.length);
+        tr[i].style.display = "";
+      }
+    }
+  }
+
+function lessThanFive(){
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      var value = td.innerHTML.substring(1, td.innerHTML.length);
+      if(value > -5 && value < 0){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
 }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+function fiveToTen(){
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      var value = td.innerHTML.substring(1, td.innerHTML.length);
+      if(value < -5 && value > -10){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+function elevenToTwenty(){
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      var value = td.innerHTML.substring(1, td.innerHTML.length);
+      if(value < -11 && value > -20){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function twentyOneToFifty(){
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      var value = td.innerHTML.substring(1, td.innerHTML.length);
+      if(value < -21 && value > -50){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function fiftyOneToHundred(){
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      var value = td.innerHTML.substring(1, td.innerHTML.length);
+      if(value < -51 && value > -100){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function greaterThanHundred(){
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      var value = td.innerHTML.substring(1, td.innerHTML.length);
+      if(value < -100){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function income(){
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      var value = td.innerHTML.substring(1, td.innerHTML.length);
+      if(value > 0){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function todaysDate(){
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0
+  var yyyy = today.getFullYear();
+
+  if(dd<10) {
+      dd = '0'+dd
+  }
+  if(mm<10) {
+      mm = '0'+mm
+  }
+
+  today = mm + '/' + dd + '/' + yyyy;
+  console.log(today);
+  return today;
+}
+
+function selectDates(value){
+  var x = document.getElementById("dates").value;
+  switch(x){
+    case "1":
+        day();
+          break;
+    case "2":
+        week();
+          break;
+    case "3":
+      month();
+          break;
+    case "4":
+      quarter();
+          break;
+    case "5":
+      year();
+          break;
+    default:
+      allDates();
+          break;
+  }
+}
+
+function day(){
+  var today = new Date(todaysDate()).getDate();
+  var thisYear = new Date(todaysDate()).getFullYear();
+  var thisMonth = new Date(todaysDate()).getMonth() + 1;
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      var value = td.innerHTML.substring(0, td.innerHTML.length);
+      var dayVal = value.substring(3,5);
+      var yearVal = value.substring(6);
+      var monthVal = value.substring(0,2);
+      if((dayVal==today)&&(thisMonth==monthVal)&&(thisYear==yearVal)){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function week(){
+  var thisYear = new Date(todaysDate()).getFullYear();
+  var thisMonth = new Date(todaysDate()).getMonth() + 1;
+  var thisWeek = new Date(todaysDate()).getDate();
+  var dayWeek = new Date(todaysDate()).getDay();
+  while(dayWeek > 0 && thisWeek > 0)
+  {
+    thisWeek = thisWeek - 1;
+    dayWeek= dayWeek - 1;
+  }
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      var value = td.innerHTML.substring(0, td.innerHTML.length);
+      var dayVal = value.substring(3,5);
+      var yearVal = value.substring(6);
+      var monthVal = value.substring(0,2);
+      if((monthVal == thisMonth) && (thisYear == yearVal) && (dayVal > thisWeek) && (dayVal < thisWeek + 7)){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function month(){
+  var thisYear = new Date(todaysDate()).getFullYear();
+  var thisMonth = new Date(todaysDate()).getMonth() + 1;
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      var value = td.innerHTML.substring(0, td.innerHTML.length);
+      var yearVal = value.substring(6);
+      var monthVal = value.substring(0,2);
+      console.log(yearVal);
+      if((monthVal == thisMonth) && (thisYear == yearVal)){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function quarter(){
+  var thisYear = new Date(todaysDate()).getFullYear();
+  var thisQtr = new Date(todaysDate()).getMonth() + 1;
+  if(thisQtr < 3) thisQtr = 1;
+  else if(thisQtr < 6) thisQtr = 3;
+  else if(thisQtr < 9) thisQtr = 6;
+  else thisQtr = 9;
+  console.log(thisYear);
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      var value = td.innerHTML.substring(0, td.innerHTML.length);
+      var yearVal = value.substring(6);
+      var qtrVal = value.substring(0,2);
+      console.log(yearVal);
+      if((qtrVal > thisQtr) && (qtrVal < (thisQtr + 3)) && (thisYear == yearVal)){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function year(){
+  var thisYear = new Date(todaysDate()).getFullYear();
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      var value = td.innerHTML.substring(0, td.innerHTML.length);
+      var yearVal = value.substring(6);
+      if(yearVal == thisYear){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function allDates(){
+  var today = todaysDate();
+  table = document.getElementById("list");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      var value = td.innerHTML.substring(0, td.innerHTML.length);
+      if(value > -5 && value < 0){
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
       }
     }
   }
