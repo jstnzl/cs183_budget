08748bb707da5187e74b56f98b00cd04e4c0a777
Justin <justinliu2015@gmail.com>
2018-05-30 13:12:16 -0700

    Merge branch 'master' of https://github.com/jtintin/CS183_Project

--- static/js/search.js
+++ static/js/search.js
@@ -1,22 +1,3 @@
-function search() {
-  var input, filter, table, tr, td, i;
-  input = document.getElementById("entry");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("list");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[2];
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      }
-      else{
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
-
 function searchingWithFilter(){
   var input = document.getElementById("entry");
   var filter = input.value.toUpperCase();
@@ -83,9 +64,6 @@ function searchingWithFilter(){
         else{
           tr[i].style.display = "none";
         }
-      }
-      else if (de.innerHTML.toUpperCase().includes(filter)){
-        tr[i].style.display = "";
       }
       else{
         tr[i].style.display = "none";
