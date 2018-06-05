64569f4f23566ba9d76fc82d7802996ba715fb2c
Josh <JTCheung3@gmail.com>
2018-06-05 12:34:01 -0700

    update

--- applications/budget/static/js/percentage.js
+++ applications/budget/static/js/percentage.js
@@ -41,3 +41,68 @@ function findPercentages(){
       }
   });
 }
+
+
+var descriptions = [];
+var prices = [];
+var dates = [];
+
+function getData() {
+  descriptions = [];
+  prices = [];
+  dates = [];
+  var table = document.getElementById("list");
+  var tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    pr = tr[i].getElementsByTagName("td")[1];
+    de = tr[i].getElementsByTagName("td")[2];
+    if(td){
+      date = td.innerHTML;
+      dates.push(date);
+    }
+    if(pr) {
+      price = pr.innerHTML.substring(1, pr.innerHTML.length);
+      prices.push(parseFloat(price).toFixed(2));
+    }
+    if(de) {
+      descript = de.innerHTML;
+      descriptions.push(descript);
+    }
+  }
+}
+
+function getTotal() {
+  getData();
+  var total = 0;
+  for(var i = 0; i < prices.length; i++) {
+    if(prices[i] < 0) {
+      total += Math.abs(prices[i]);
+      //console.log(prices[i]);
+    }
+  }
+  return parseFloat(total).toFixed(2);
+}
+
+function getItemTotal() {
+  getData();
+  var itemTotal = 0;
+  var input = document.getElementById("search");
+  var filter = input.value.toUpperCase();
+  for(var i = 0; i < descriptions.length; i++) {
+    if(descriptions[i].toUpperCase().includes(filter)){
+      if(prices[i] < 0) {
+        itemTotal += Math.abs(prices[i]);
+      }
+    }
+  }
+  return parseFloat(itemTotal).toFixed(2);
+}
+
+function findPercent() {
+  var itemTotal = getItemTotal();
+  var total = getTotal();
+  var percentage = itemTotal/total;
+  console.log(percentage*100);
+  return (percentage*100);
+}
